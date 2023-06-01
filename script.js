be652494fd7bbf57bc90a009818b1418258a9433
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-06-01 15:17:02 +0200

    Img done

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            helloWorld : "Hello World!"
+            helloWorld : "Hail Tiamat!",
+            myImg : "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bf1243dc-5053-4695-ae36-551a6174282a/dd3k9kw-8b47f3f6-9748-420d-962a-c6f63b1b3f29.jpg/v1/fill/w_1280,h_772,q_75,strp/tiamat__the_goddess_of_evil_dragons__commission__by_dragolisco_dd3k9kw-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzcyIiwicGF0aCI6IlwvZlwvYmYxMjQzZGMtNTA1My00Njk1LWFlMzYtNTUxYTYxNzQyODJhXC9kZDNrOWt3LThiNDdmM2Y2LTk3NDgtNDIwZC05NjJhLWM2ZjYzYjFiM2YyOS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.mMv64f_o-VsUmJBiZ8RWqEu77X4bdbEDvJmbvJ9cMt4",
         }
     }
 }).mount("#vue")
